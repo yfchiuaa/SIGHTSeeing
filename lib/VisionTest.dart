f778c5e23b14d8512e4b0352aa2e280c8e460946
BoscoTin <tintin111198@gmail.com>
2019-03-10 21:31:09 +0800

    Finish page simple layout

--- lib/VisionTest.dart
+++ lib/VisionTest.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'NoKeyboardTextField.dart';
+import 'package:flutter/cupertino.dart';
 
 class VisionTest extends StatefulWidget{
   @override
@@ -7,104 +7,32 @@ class VisionTest extends StatefulWidget{
 }
 
 class _VisionTestState extends State<VisionTest> {
-  final double KEY_HEIGHT = 60;
-  final double TEXT_HEIGHT = 40;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          // divide the whole area by two half
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    _showingText("", TEXT_HEIGHT, 0.4, TextAlign.center),
-                    _showingText("左", TEXT_HEIGHT, 0.3, TextAlign.left),
-                    _showingText("右", TEXT_HEIGHT, 0.3, TextAlign.left),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    _showingText("裸眼远视力", TEXT_HEIGHT, 0.4, TextAlign.center),
-                    _inputTextField(TEXT_HEIGHT, 0.3),
-                    _inputTextField(TEXT_HEIGHT, 0.3),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    _showingText("戴镜远视力", TEXT_HEIGHT, 0.4, TextAlign.center),
-                    _inputTextField(TEXT_HEIGHT, 0.3),
-                    _inputTextField(TEXT_HEIGHT, 0.3),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    _showingText("小孔视力", TEXT_HEIGHT, 0.4, TextAlign.center),
-                    _inputTextField(TEXT_HEIGHT, 0.3),
-                    _inputTextField(TEXT_HEIGHT, 0.3),
-                  ],
-                ),
-              ]
-            ),
-
-            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                /// number keyboard
-                Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        _keyboardButton("1", KEY_HEIGHT, 0.2),
-                        _keyboardButton("2", KEY_HEIGHT, 0.2),
-                        _keyboardButton("3", KEY_HEIGHT, 0.2),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        _keyboardButton("4", KEY_HEIGHT, 0.2),
-                        _keyboardButton("5", KEY_HEIGHT, 0.2),
-                        _keyboardButton("6", KEY_HEIGHT, 0.2),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        _keyboardButton("7", KEY_HEIGHT, 0.2),
-                        _keyboardButton("8", KEY_HEIGHT, 0.2),
-                        _keyboardButton("9", KEY_HEIGHT, 0.2),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        _keyboardButton("", KEY_HEIGHT, 0.2),
-                        _keyboardButton("0", KEY_HEIGHT, 0.2),
-                        _keyboardButton("", KEY_HEIGHT, 0.2),
-                      ],
-                    ),
-                  ],
-                ),
-                /// special keys
-                Column(
-                  children: <Widget>[
-                    _specialButton(Icons.backspace, KEY_HEIGHT, 0.4),
-                    _keyboardButton(".", KEY_HEIGHT * 2, 0.4),
-                    _specialButton(Icons.add, KEY_HEIGHT, 0.4),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+  final double TEXT_HEIGHT = 60;
+/*
+  Future<bool> _onBackPressed() {
+    return showDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text("确定要保存及退出吗?"),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('保存'),
+            onPressed: (){
+              _saveData();
+              Navigator.of(context).pop(true);
+            },
+          ),
+          FlatButton(
+            child: Text('不要'),
+            onPressed: (){
+              Navigator.of(context).pop(false);
+            },
+          ),
+        ],
       ),
-    );
+    )??false;
   }
-
+*/
   SizedBox _showingText( String text, double boxHeight, double ratio, TextAlign alignment){
     double screenWidth = MediaQuery.of(context).size.width;
     return SizedBox(
@@ -119,45 +47,105 @@ class _VisionTestState extends State<VisionTest> {
     );
   }
 
-  SizedBox _inputTextField( double boxHeight, double ratio){
+  SizedBox _inputTextField( double boxHeight, double ratio, TextEditingController _controller){
     double screenWidth = MediaQuery.of(context).size.width;
     return SizedBox(
       width: ratio * screenWidth,
       height: boxHeight,
-      child: NoKeyboardEditableText(
-          controller: TextEditingController(
-            text: "number",
-          ),
-          style: TextStyle(
-            color: Color.fromRGBO(0, 0, 0, 1),
-          ),
-          cursorColor: Color.fromRGBO(0, 0, 0, 0.9)),
-    );
-  }
-
-  SizedBox _keyboardButton( String text, double keyHeight , double ratio){
-    double screenWidth = MediaQuery.of(context).size.width;
-    return SizedBox(
-      width: ratio * screenWidth,
-      height: keyHeight,
-      child: RaisedButton(
-        padding: EdgeInsets.all(3),
-        onPressed: (){},
-        child: Text(text),
+      child: TextField(
+        controller: _controller,
+        keyboardType: TextInputType.numberWithOptions(decimal: true),
+        textInputAction: TextInputAction.next,
       ),
     );
   }
 
-  SizedBox _specialButton( IconData icon, double keyHeight , double ratio){
-    double screenWidth = MediaQuery.of(context).size.width;
-    return SizedBox(
-      width: ratio * screenWidth,
-      height: keyHeight,
-      child: RaisedButton(
-        padding: EdgeInsets.all(3),
-        onPressed: () {},
-        child: Icon(icon),
-      ),
+  @override
+  Widget build(BuildContext context) {
+    final List<TextEditingController> _controller = [];
+    for(int i = 0; i < 6; i++)
+      _controller.add( new TextEditingController());
+
+    //return WillPopScope(
+      //onWillPop: _onBackPressed,
+      //child: Scaffold(
+    return Scaffold(
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _showingText("", TEXT_HEIGHT, 0.4, TextAlign.center),
+                  _showingText("左", TEXT_HEIGHT, 0.2, TextAlign.center),
+                  SizedBox(width: MediaQuery.of(context).size.width*0.05),
+                  _showingText("右", TEXT_HEIGHT, 0.2, TextAlign.center),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _showingText("裸眼远视力", TEXT_HEIGHT, 0.4, TextAlign.center),
+                  _inputTextField(TEXT_HEIGHT, 0.2, _controller[0]),
+                  SizedBox(width: MediaQuery.of(context).size.width*0.05),
+                  _inputTextField(TEXT_HEIGHT, 0.2, _controller[1]),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _showingText("戴镜远视力", TEXT_HEIGHT, 0.4, TextAlign.center),
+                  _inputTextField(TEXT_HEIGHT, 0.2, _controller[2]),
+                  SizedBox(width: MediaQuery.of(context).size.width*0.05),
+                  _inputTextField(TEXT_HEIGHT, 0.2, _controller[3]),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _showingText("小孔视力", TEXT_HEIGHT, 0.4, TextAlign.center),
+                  _inputTextField(TEXT_HEIGHT, 0.2, _controller[4]),
+                  SizedBox(width: MediaQuery.of(context).size.width*0.05),
+                  _inputTextField(TEXT_HEIGHT, 0.2, _controller[5]),
+                ],
+              ),
+
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
+              Row(
+                children: <Widget>[
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.4),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    child: RaisedButton(
+                      onPressed: (){
+                        /// button for save and back to last page
+                        _saveData();
+                        Navigator.pop(context);
+                      },
+                      child: Text("完成"),
+                    ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    child: RaisedButton(
+                        onPressed: (){
+                          for(int i = 0; i < _controller.length; i++)
+                            _controller[i].clear();
+                        },
+                      child: Text("清除所有"),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+        ),
+      //),
     );
+  }
+  void _saveData(){
+
   }
 }
